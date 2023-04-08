857275baeb8a102a32eb64e1c4bcaeb63cd59309
lissavcasas <lissavcasas@gmail.com>
2023-04-07 22:35:52 -0500

    handle message errors

--- src/main/java/pe/com/cibertec/web/ControladorInicio.java
+++ src/main/java/pe/com/cibertec/web/ControladorInicio.java
@@ -1,27 +1,34 @@
 package pe.com.cibertec.web;
 
 import java.util.Collection;
+import javax.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.ConversionFailedException;
 import org.springframework.data.repository.query.Param;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import pe.com.cibertec.domain.Cliente;
 import pe.com.cibertec.servicio.ClienteService;
 
 @Controller
 @Slf4j
 public class ControladorInicio {
+
     @Autowired
     private ClienteService clienteService;
 
-    @GetMapping("/")
-    public String inicio(Model model,@Param("palabra")String palabra, @AuthenticationPrincipal User user) {
+    @GetMapping("/clientes")
+    public String inicio(Model model, @Param("palabra") String palabra, @AuthenticationPrincipal User user) {
         Collection<? extends GrantedAuthority> currentUserRoles = user.getAuthorities();
         log.info("User rol:" + currentUserRoles);
         boolean isAdmin = currentUserRoles.stream()
@@ -35,11 +42,10 @@ public class ControladorInicio {
             model.addAttribute("mostrarBoton", false);
         }
 
-        
         var clientes = clienteService.listarClientes(palabra);
-        
+
         model.addAttribute("clientes", clientes);
-        model.addAttribute("palabra",palabra);
+        model.addAttribute("palabra", palabra);
         return "clientes";
     }
 
@@ -49,14 +55,26 @@ public class ControladorInicio {
     }
 
     @PostMapping("/guardar")
-    public String guardar(Cliente cliente) {
-        clienteService.guardar(cliente);
-        return "redirect:/";
+    public String guardar(@Valid Cliente cliente, BindingResult result, Model model) throws BindException {
+
+        if (result.hasErrors()) {
+            String mensaje = "Los siguientes campos presentan errores: ";
+            for (FieldError error : result.getFieldErrors()) {
+                mensaje += error.getField();
+            }
+            model.addAttribute("error", mensaje);
+            return "actualizar";
+        } else {
+            clienteService.guardar(cliente);
+            String mensaje = "Cliente con código Nro. " + cliente.getIdeCli() + " guardado con éxito.";
+            model.addAttribute("mensaje", mensaje);
+            return "actualizar";
+        }
     }
 
     @GetMapping("/editar/{ideCli}")
     public String editar(Cliente cliente, Model model) {
-        cliente = clienteService.encontrarCliente(cliente);  
+        cliente = clienteService.encontrarCliente(cliente);
         model.addAttribute("cliente", cliente);
         return "actualizar";
     }
@@ -64,6 +82,6 @@ public class ControladorInicio {
     @GetMapping("/eliminar/{ideCli}")
     public String eliminar(Cliente cliente) {
         clienteService.eliminar(cliente);
-        return "redirect:/";
+        return "redirect:/clientes";
     }
 }
